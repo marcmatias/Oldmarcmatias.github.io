7f049f798bc15248f16e9dbe7a3a73432679480b
Marcel Marques <marcmatiasdev@gmail.com>
2022-06-18 01:04:18 -0300

    Updating backbutton styles and back icon

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,10 @@
 <template>
 	<g-link class="logo" to="/">
+	  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" fill="currentColor" viewBox="0 0 16 15">
+		<path stroke="#ced8de" stroke-width="1" fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+	  </svg>
 	  <span class="logo__text">
-	    &larr; {{ $static.metadata.siteName }}
+		{{ $static.metadata.siteName }}
 	  </span>
 	</g-link>
 </template>
@@ -19,6 +22,14 @@ query {
 	text-decoration: none;
   color: var(--body-color)!important;
   font-size: .9em;
+  padding: .2em .8em;
+  background-color: transparent !important;
+  display: flex;
+  align-items: center;
+
+	svg {
+		margin-right: .5rem;
+	}
 
 	&__image {
 		vertical-align: middle;
